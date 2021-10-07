bba3d718a068d883a4255b0b7bc25255b75e7a06
realfish07 <realfish07@gmail.com>
2021-10-07 17:50:06 +0800

    fix: 修正base設定錯誤

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,26 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
-    plugins: [vue()],
-    base: process.env.PROD ? '/vue-calculator/' : '/',
-    server: {
-      port: 8081,
-      open: true,
-      host: '0.0.0.0',
-      hmr: {
-        overlay: true // display server error overlay
-      }
-    },
-    resolve: {
-      alias: [
-        {
-          find: '@',
-          replacement: resolve(__dirname, 'src')
-        }
-      ]
+export default defineConfig({
+  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? '/vue-calculator/' : '/',
+  server: {
+    port: 8081,
+    open: true,
+    host: '0.0.0.0',
+    hmr: {
+      overlay: true // display server error overlay
     }
-  })
-}
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      }
+    ]
+  }
+})
+
